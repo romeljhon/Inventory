29c5d35a742e57541d3a832887c182de5572627b
romeljhon salvaleon <pajojhon@gmail.com>
2025-11-06 05:24:55 +0000

    Console Error
    FirebaseError: Firebase: Error (auth/unauthorized-domain

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,9 +3,10 @@
 // IMPORTANT: Replace this with the actual config from your Firebase project settings.
 export const firebaseConfig = {
   apiKey: "AIzaSyAfxV-LBtzoAUasbRZRTzGhBRw5CW4pnb0",
-  authDomain: "stockwise-plf8a.firebaseapp.com",
+  authDomain: "stockwise-plf8a.web.app",
   projectId: "stockwise-plf8a",
-  storageBucket: "stockwise-plf8a.firebasestorage.app",
+  storageBucket: "stockwise-plf8a.appspot.com",
   messagingSenderId: "434763397253",
   appId: "1:434763397253:web:209bcde47be19a78d651ec"
 };
+
